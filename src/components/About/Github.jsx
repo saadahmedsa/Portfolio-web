8d2adee13782d13f30d834d751d315a77d38549d
Saad Ahmed <153908155+saadahmedsa@users.noreply.github.com>
2025-09-13 02:25:45 -0700

    Update Github.jsx

--- src/components/About/Github.jsx
+++ src/components/About/Github.jsx
@@ -8,7 +8,7 @@ function Github() {
         Days I <span className="text-purple-500">Code</span>
       </h1>
       <GitHubCalendar
-        username="SaadAhmedSA"
+        username="saadahmedsa"
         blockSize={10}
         blockMargin={5}
         color="#c084f5"
