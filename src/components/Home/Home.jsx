13ebf9925aa790c18129641168b8fc72e267b6be
saadahmedsa <ss5099903@gmail.com>
2025-09-18 23:28:52 -0700

    Update home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,15 +7,17 @@ import Type from "./Type";
 function Home() {
   return (
     <section className="relative w-full min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
-      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto my-auto px-4 flex flex-col md:flex-row items-center justify-between">
         <div className="md:w-7/12 text-left md:text-left">
-          <h1 className="text-3xl font-bold mb-4">
-            Hi There! <span role="img" aria-labelledby="wave">👋🏻</span>
+          <h1 className="text-3xl font-semiboldbold mb-4">
+            Hi There!  <span className="wave" role="img" aria-labelledby="wave">
+                👋🏻
+              </span>
           </h1>
-          <h1 className="text-3xl font-extrabold text-purple-500">
-            I'M <span className="text-white">SAAD AHMED</span>
+          <h1 className="text-4xl font-bold text-purple-500">
+            I'M <span className="text-white  ">SAAD AHMED</span>
           </h1>
-          <div className="mt-6 text-left  ">
+          <div className="mt-6 text-xl text-left  ">
             <Type />
           </div>
         </div>
